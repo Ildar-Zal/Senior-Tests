525b962eed0bc277aa6fbf9238f71c21217a0412
Ildar Zalyalov <id10997@yandex.ru>
2026-02-28 11:33:14 +0300

    in progress

---
+++ src/main/java/models/LoginUserRequest.java
@@ -0,0 +1,15 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginUserRequest {
+    private String userName;
+    private String password;
+}
