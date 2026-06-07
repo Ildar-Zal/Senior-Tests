676ba85616413fcb25f3233d4d26d106ec9fad51
Ildar Zalyalov <id10997@yandex.ru>
2026-06-07 17:19:47 +0300

    Debug pipeline

--- src/main/java/ui/pages/UserDashboard.java
+++ src/main/java/ui/pages/UserDashboard.java
@@ -48,7 +48,7 @@ public class UserDashboard extends BasePage<UserDashboard> {
     }
 
     public TransferPage testMethod() {
-        return new TransferPage();//12
+        return new TransferPage();//123
     }
 
 }
